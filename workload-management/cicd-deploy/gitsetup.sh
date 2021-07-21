dcdbc74f36d4e33ec13dd4804a56fbd86efc1261
Allamand Sebastien <sallaman@amazon.com>
2021-07-21 18:31:37 +0200

    fix authent on codecommit repo

--- workload-management/cicd-deploy/gitsetup.sh
+++ workload-management/cicd-deploy/gitsetup.sh
@@ -3,7 +3,8 @@ cd app
 rm -Rf gitrepo
 mkdir gitrepo
 cd gitrepo
-git clone $URL .
+#git clone $URL .
+git clone codecommit://ecs-anywhere-repo .
 cp -Rf ../Dockerfile ../index.html .
 git add .
 git commit -m "Initial commit"
